5d766f2f27e2a82fc3cb7315219711d07b715451
haeaxo <hae@axogroup.com>
2024-07-26 14:16:50 +0100

    Docker support

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,22 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { Task } from '@/tasks/entities/task.entity';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT) || 3306,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [User, Task],
+
+  synchronize: true,
+};
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nest_project',
-      entities: [User, Task],
-      synchronize: true,
-    }),
-    UsersModule,
-    TasksModule,
-  ],
+  imports: [TypeOrmModule.forRoot(config), UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
